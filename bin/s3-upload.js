32157283fc1fc6bb529c661461d0aa9006725c87
acobrelo <acobrelo@gmail.com>
2016-07-08 15:29:10 -0400

    Refactored into module
    This app is useable from both a command line and an express api server

--- bin/s3-upload.js
+++ bin/s3-upload.js
@@ -1,37 +1,10 @@
 'use strict';
 
+require('dotenv').config();
+
+const uploader = require('../lib/aws-s3-upload');
+
 const fs = require('fs');
-const fileType = require('file-type');
-const AWS = require('aws-sdk');
-const crypto = require('crypto');
-
-const s3 = new AWS.S3({
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  },
-});
-
-const mimeType = (data) => {
-  return Object.assign({
-    ext: 'bin',
-    mime: 'application/octet-stream'
-  }, fileType(data));
-  //{} on left gets pushed into {} on right
-};
-//so we write this in to take in a buffer and return an object
-
-const randomHexString = (length) => {
-  return new Promise((resolve, reject) => {
-    crypto.randomBytes(length), (error, buffer) => {
-      if (error) {
-        reject(error);
-      }
-
-      resolve('buffer'.toString);
-    }
-  });
-};
 
 let filename = process.argv[2] || '';
 //reminder: this is how you get stuff FROM the command line
@@ -42,51 +15,13 @@ const readFile = (filename) => {
       if (error) {
         reject(error);
       }
+
       resolve(data);
     });
   });
-};
-
-const awsUpload = (file) => {
-  return randomHexString(16)
-  .then((filename) => {
-  return {
-    ACL: "public-read",
-    Body: file.data,
-    Bucket: '1.ga.wdi.12.abl',
-    ContentType: file.mime,
-    Key: `test/test.${file.ext}`,
-  };
-})
-.then((options) => {
-  return new Promise((resolve, reject) => {
-    s3.upload(options, (error, data) => {
-      if (error) {
-        reject(error);
-      }
-      resolve(data);
-    });
-  });
-});
-
-
-  //return Promise.resolve(options);
-  //return options
 };
 
 readFile(filename)
-.then((data) => {
-  let file = mimeType(data);
-  //custom object, file, mimeType, looks at the ext and headers
-  //of a buffer to determine content type....
-  //readfile gives us back a buffer
-  //used to be an object file = {}, function needs to return an object for the
-  //extra shit to work
-  file.data = data;
-  //pass cust obj the data
-  return file;
-})
-//.then(awsUpload)
+.then(uploader.awsUpload)
 .then(console.log)
-//.then((data) => console.log(`${filename} is ${data.length} bytes long`))
 .catch(console.error);
