ff1c3372e3cbcd6851bdfcb3acb73941adc33b01
acobrelo <acobrelo@gmail.com>
2016-07-08 13:11:45 -0400

    Determined file/mime-type and start upload func
    Use the file-type module to determine to content-type of the upload

--- bin/s3-upload.js
+++ bin/s3-upload.js
@@ -1,6 +1,16 @@
 'use strict';
 
 const fs = require('fs');
+const fileType = require('file-type');
+
+const mimeType = (data) => {
+  return Object.assign({
+    ext: 'bin',
+    mime: 'application/octet-stream'
+  }, fileType(data));
+  //{} on left gets pushed into {} on right
+};
+//so we write this in to take in a buffer and return an object
 
 let filename = process.argv[2] || '';
 //reminder: this is how you get stuff FROM the command line
@@ -16,6 +26,31 @@ const readFile = (filename) => {
   });
 };
 
+const awsUpload = (file) => {
+  const options = {
+    ACL: "public-read",
+    Body: file.data,
+    Bucket: '1.ga.wdi.12.abl',
+    ContentType: file.mime,
+    Key: 'test/test.${file.ext}'
+  };
+  return Promise.resolve(options);
+  //return options
+};
+
 readFile(filename)
-.then((data) => console.log(`${filename} is ${data.length} bytes long`))
+.then((data) => {
+  let file = mimeType(data);
+  //custom object, file, mimeType, looks at the ext and headers
+  //of a buffer to determine content type....
+  //readfile gives us back a buffer
+  //used to be an object file = {}, function needs to return an object for the
+  //extra shit to work
+  file.data = data;
+  //pass cust obj the data
+  return file;
+})
+//.then(awsUpload)
+.then(console.log)
+//.then((data) => console.log(`${filename} is ${data.length} bytes long`))
 .catch(console.error);
